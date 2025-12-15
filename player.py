bf3278ec7fea6ecd98b7d19a31bac636115a9191
thepyroclast <colin555huff@gmail.com>
2025-12-15 15:56:55 -0700

    created move function to let the player move

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 #definition of the player class
 from circleshape import CircleShape
 from constants import PLAYER_RADIUS
-from constants import LINE_WIDTH, PLAYER_TURN_SPEED
+from constants import LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
 import pygame
 
 class Player(CircleShape):
@@ -31,6 +31,17 @@ class Player(CircleShape):
             self.rotate(reverse_dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+        
 
     
 
