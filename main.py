77fdfd5e17e14bc8c40d1263b7324e5cd538e423
thepyroclast <colin555huff@gmail.com>
2025-12-15 15:07:11 -0700

    created player.py to add the player to the map

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 
@@ -8,6 +9,7 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -19,6 +21,7 @@ def main():
              if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         max_fps.tick(60)
         dt = max_fps.tick(60) / 1000
