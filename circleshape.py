7bfc92f7842bc5d736084b95631878c72be4ec90
thepyroclast <colin555huff@gmail.com>
2025-12-26 14:08:11 -0700

    updated the program to run more smoothly

--- circleshape.py
+++ circleshape.py
@@ -3,7 +3,7 @@ import pygame
 
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius, *groups):
         # we will be using this later
         if hasattr(self, "containers"):
             super().__init__(self.containers)
--- main.py
+++ main.py
@@ -10,23 +10,30 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    max_fps = pygame.time.Clock()
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     dt = 0
-    while 1:
+    while True:
         log_state()
         for event in pygame.event.get():
              if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player1.update(dt)
-        player1.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
-        max_fps.tick(60)
-        dt = max_fps.tick(60) / 1000
+        fps = clock.tick(60)
+        dt = fps / 1000
         
 
 if __name__ == "__main__":
