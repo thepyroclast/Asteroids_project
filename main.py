b0bc209c1acf279393ca7a01f4e6ca6b25d1cdb7
thepyroclast <colin555huff@gmail.com>
2025-12-29 15:18:23 -0700

    added asteroid.py, creating asteroids and gave both asteroids and player hitboxes and addedd asteroidfield.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from logger import log_event
+import sys
 
 
 
@@ -17,9 +21,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     
     dt = 0
     while True:
@@ -28,6 +36,11 @@ def main():
              if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for asteroid in asteroids:
+            if asteroid.collides_with(player) == True:
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         updatable.update(dt)
         for item in drawable:
             item.draw(screen)
