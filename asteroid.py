234ea0d9b8d57a122e1ccfd09e803dbe82ac3d60
thepyroclast <colin555huff@gmail.com>
2025-12-30 13:40:54 -0700

    added .split() method to asteroid class making them split on destruction

--- asteroid.py
+++ asteroid.py
@@ -1,8 +1,10 @@
 #the file containing the asteroids
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
- 
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
+import random
+
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
@@ -13,5 +15,18 @@ class Asteroid(CircleShape):
     def update(self, dt):
          self.position += self.velocity * dt
          
-    
+    def split(self):
+         self.kill()
+         if self.radius <= ASTEROID_MIN_RADIUS:
+              return
+         log_event("asteroid_split")
+         angle = random.uniform(20, 50)
+         asteroid_angle_1 = self.velocity.rotate(angle)
+         asteroid_angle_2 = self.velocity.rotate(-angle)
+         new_radius = self.radius - ASTEROID_MIN_RADIUS
+         asteroid_1 = Asteroid(self.position[0], self.position[1], new_radius)
+         asteroid_2 = Asteroid(self.position[0], self.position[1], new_radius)
+         asteroid_1.velocity = asteroid_angle_1 * 1.2
+         asteroid_2.velocity = asteroid_angle_2 * 1.2
+
 
--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
             for shot in shots:
                 if asteroid.collides_with(shot) == True:
                     log_event("asteroid_shot")
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         updatable.update(dt)
         for item in drawable:
